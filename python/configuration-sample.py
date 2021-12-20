61f102594a435bf0b356327e283126742af6da2e
Fred Morris <consulting@m3047.net>
2021-12-20 11:25:58 -0800

    ENH: Console for examination of in-memory data structures.

--- python/configuration-sample.py
+++ python/configuration-sample.py
@@ -29,3 +29,8 @@ RESPONSE_POLICY_ZONE = 'rpz.example.com'
 # Size of the RPZ.
 # CACHE_SIZE = 10000
 CACHE_SIZE = None
+
+# The agent can run a console which provides visibility into internal data
+# structures. Set this to a dict with 'host' and 'port' keys.
+CONSOLE = None
+# CONSOLE = dict(host='127.0.0.1', port=3047)
